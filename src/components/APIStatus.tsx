81cf92a518e69bda7d90c4ef779cc4553433e629
Squizlee <ethanscavia777@gmail.com>
2021-03-28 16:21:15 +1100

    APIStatus now only calls getStatus once on mount

--- src/components/APIStatus.tsx
+++ src/components/APIStatus.tsx
@@ -1,46 +1,46 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const APIStatus = () => {
-
 	const STATES = {
 		LOADING: "LOADING",
 		SUCCESS: "SUCCESS",
-		ERROR: "ERROR"
+		ERROR: "ERROR",
 	};
 	const [state, setCurrState] = useState(STATES.LOADING);
 
-	async function getStatus(){
+	function getStatus() {
 		const responses = {
-			SUCCESS: "spacetraders is currently online and available to play"
+			SUCCESS: "spacetraders is currently online and available to play",
 		};
-		const response = await axios.get("https://api.spacetraders.io/game/status")
-		switch (response.data.status) {
-			case responses.SUCCESS:
-				setCurrState(STATES.SUCCESS);
-				break;
-			default: 
-				setCurrState(STATES.ERROR)
-		}
+		axios
+			.get("https://api.spacetraders.io/game/status")
+			.then((response) => {
+				switch (response.data.status) {
+					case responses.SUCCESS:
+						setCurrState(STATES.SUCCESS);
+						break;
+					default:
+						setCurrState(STATES.ERROR);
+				}
+			})
+			.catch((err) => console.error(err));
 	}
-	getStatus();
+	// eslint-disable-next-line
+	useEffect(() => getStatus(), []);
 
-	switch(state){
+	switch (state) {
 		case "LOADING":
-			return(
-				<p>Loading</p>
-			);
+			return <p>Loading</p>;
 		case "SUCCESS":
-			return(
-				<p>SpaceTraders is Online 🟢</p>
-			);
+			return <p>SpaceTraders is Online 🟢</p>;
 		case "ERROR":
-			return(
+			return (
 				<p>SpaceTraders is Offline, try again in a couple of hours</p>
 			);
-		default: 
-			return <p>Loading</p>
+		default:
+			return <p>Loading</p>;
 	}
-}
+};
 
 export default APIStatus;
